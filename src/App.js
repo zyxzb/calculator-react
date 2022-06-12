8b73b2d3da47ee5c80a9032ebeba0060a9f39771
Bartek Z <bartekzarzyk@gmail.com>
2022-06-13 00:09:29 +0200

    add calculator to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+
+  }
+
+  const handleClear = () => {
+    setResult('');
+
+  }
+
+  const handleRemove = () => {
+    setResult(result.slice(0, -1));
+
+  }
+ 
+  const handleCalculate = () => {
+      try {
+        setResult(eval(result).toString());
+      } catch {
+        setResult('use one operator');
+      }
+  }
+
+    return (
+            <div className="wrapper">
+                  <input type="text" 
+                  className="screen calc-numbers" 
+                  value={result}/>
+                  <div className="buttons">
+                    <button onClick={handleClear} name="" className="button clear">C</button>
+                    <button onClick={handleRemove} className="button opt">&larr;</button>
+                    <button onClick={handleClick} name="/" className="button opt">&divide;</button>
+                    <button onClick={handleClick} name="7" className="button">7</button>
+                    <button onClick={handleClick} name="8" className="button">8</button>
+                    <button onClick={handleClick} name="9" className="button">9</button>
+                    <button onClick={handleClick} name="*" className="button opt">X</button>
+                    <button onClick={handleClick} name="4" className="button">4</button>
+                    <button onClick={handleClick} name="5" className="button">5</button>
+                    <button onClick={handleClick} name="6" className="button">6</button>
+                    <button onClick={handleClick} name="-"className="button opt">-</button>
+                    <button onClick={handleClick} name="1" className="button">1</button>
+                    <button onClick={handleClick} name="2" className="button">2</button>
+                    <button onClick={handleClick} name="3" className="button">3</button>
+                    <button onClick={handleClick} name="+" className="button opt">+</button>
+                    <button onClick={handleClick} name="0" className="button zero">0</button>
+                    <button onClick={handleCalculate} name="=" className="button equals">=</button>
+                  </div>
+            </div>
+    );
 }
 
 export default App;
